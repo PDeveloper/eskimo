c4fce25bd7e72dc5616a6d4d9fe7c8a62d4dead9
ashes999 <ashes999@yahoo.com>
2016-03-29 16:40:14 -0400

    Unit tests for Entity

--- test/TestSuite.hx
+++ test/TestSuite.hx
@@ -1,6 +1,6 @@
 import massive.munit.TestSuite;
 
-import ExampleTest;
+import eskimo.EntityTest;
 
 /**
  * Auto generated Test Suite for MassiveUnit.
@@ -14,6 +14,6 @@ class TestSuite extends massive.munit.TestSuite
 	{
 		super();
 
-		add(ExampleTest);
+		add(eskimo.EntityTest);
 	}
 }
